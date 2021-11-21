2f8cf5c18cd47f2aadf88a88059c8a2512d27034
affi031 <afnanshaikh98@gmail.com>
2021-11-21 21:45:15 +0530

    UseState()

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [count,setCount]= useState(0);
+ 
+//Destructing...
+
+function addToCart() {
+ 
+  setCount(count +1);
+  
+}
+function removefromcart() {
+ 
+  setCount(count -1);
+  
+}
+if(count<0)
+{
+  setCount(0);
+}
+return (
+  <div>
+      <h1>{count}</h1>
+     
+      <button onClick={addToCart}>Add to Cart</button>
+      <button onClick={removefromcart}>Remove from Cart</button>
+  </div>
+  //document.getElementById("root")
+);
+
 }
 
 export default App;
